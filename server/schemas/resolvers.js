633e112a29284e51525374721154b99533e6d913
deepwubs <pchucklambs@gmail.com>
2023-11-01 22:12:19 -0500

    added query and mutation to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,8 +2,17 @@ const { User } = require('../models')
 
 module.exports = {
   Query: {
-    users: async () => {
-      return await User.find({})
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new Error('You need to be logged in!');
     },
   },
+  Mutation: {
+    login: async (partent, { email, password }, context, index) => {
+      const user = await User.findOne({ email });
+      console.log(user)
+    }
+  }
 }
